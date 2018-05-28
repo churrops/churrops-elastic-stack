20aeb707665a176fe6d4299bc684ce9875844ed3
Rodrigo Floriano de Souza <rsouza@neoassist.com>
2018-05-28 13:52:48 -0300

    em desenvolvimento

--- terraform/modules/aws_security_group/vars.tf
+++
@@ -1,7 +0,0 @@
-/*
-Author: Raphael Rabelo de Oliveira
-GitHub: https://github.com/rabeloo
-*/
-/*Variaveis que podem ser recebidas pelo modulo como parametro*/
-variable "tag_name" { default = "mySG" }
-variable "vpc_id"   { default = ""}
